fa6deb91316361227b4defb83bdca17ee07e2459
Muhaiminur  Rahman <muhaiminurabir@gmail.com>
2018-09-20 14:52:30 +0600

    Final

---
+++ app/src/main/java/com/muhaiminurabir/pocketlock/MainActivity.java
@@ -0,0 +1,379 @@
+package com.muhaiminurabir.pocketlock;
+
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.app.admin.DevicePolicyManager;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.afollestad.materialdialogs.DialogAction;
+import com.afollestad.materialdialogs.MaterialDialog;
+import com.fxn.stash.Stash;
+import com.github.javiersantos.materialstyleddialogs.MaterialStyledDialog;
+import com.github.javiersantos.materialstyleddialogs.enums.Style;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+import com.facebook.ads.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity{
+    @BindView(R.id.input_password)
+    EditText inputPassword;
+    @BindView(R.id.password_lock)
+    Button passwordLock;
+    @BindView(R.id.screen_off)
+    Button screenOff;
+    public static final int RESULT_ENABLE = 11;
+    DevicePolicyManager devicePolicyManager;
+    private ActivityManager activityManager;
+    private ComponentName compName;
+
+
+    private final String TAG = MainActivity.class.getSimpleName();
+    private LinearLayout nativeAdContainer;
+    private LinearLayout adView;
+    private NativeAd nativeAd;
+
+    private InterstitialAd interstitialAd;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        try {
+            Stash.init(this);
+            inputPassword.setText(Stash.getString("password"));
+            devicePolicyManager = (DevicePolicyManager) getSystemService(DEVICE_POLICY_SERVICE);
+            activityManager = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
+            compName = new ComponentName(this, MyAdmin.class);
+            boolean active = devicePolicyManager.isAdminActive(compName);
+            if (active) {
+            } else {
+                check();
+            }
+
+            loadNativeAd();
+            interstitialAd = new InterstitialAd(this, "1813750035346387_1813751348679589");
+            // Set listeners for the Interstitial Ad
+            interstitialAd.setAdListener(new InterstitialAdListener() {
+                @Override
+                public void onInterstitialDisplayed(Ad ad) {
+                    // Interstitial ad displayed callback
+                    Log.e(TAG, "Interstitial ad displayed.");
+                }
+
+                @Override
+                public void onInterstitialDismissed(Ad ad) {
+                    // Interstitial dismissed callback
+                    Log.e(TAG, "Interstitial ad dismissed.");
+                }
+
+                @Override
+                public void onError(Ad ad, AdError adError) {
+                    // Ad error callback
+                    Log.e(TAG, "Interstitial ad failed to load: " + adError.getErrorMessage());
+                }
+
+                @Override
+                public void onAdLoaded(Ad ad) {
+                    // Interstitial ad is loaded and ready to be displayed
+                    Log.d(TAG, "Interstitial ad is loaded and ready to be displayed!");
+                    // Show the ad
+                    interstitialAd.show();
+                }
+
+                @Override
+                public void onAdClicked(Ad ad) {
+                    // Ad clicked callback
+                    Log.d(TAG, "Interstitial ad clicked!");
+                }
+
+                @Override
+                public void onLoggingImpression(Ad ad) {
+                    // Ad impression logged callback
+                    Log.d(TAG, "Interstitial ad impression logged!");
+                }
+            });
+
+            // load the ad
+            interstitialAd.loadAd();
+        } catch (Exception e) {
+            Log.d("Error Line Number", Log.getStackTraceString(e));
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        boolean isActive = devicePolicyManager.isAdminActive(compName);
+        try {
+            devicePolicyManager.setPasswordMinimumLength(compName, 0);
+            boolean result = devicePolicyManager.resetPassword("", DevicePolicyManager.RESET_PASSWORD_REQUIRE_ENTRY);
+        } catch (Exception e) {
+            Log.d("Error Line Number", Log.getStackTraceString(e));
+        }
+    }
+
+    /*@Override
+    public void onClick(View view) {
+        if (view == lock) {
+            Log.d("LOCKING", "NOW");
+            boolean active = devicePolicyManager.isAdminActive(compName);
+
+            if (active) {
+                devicePolicyManager.lockNow();
+            } else {
+                Toast.makeText(this, "You need to enable the Admin Device Features", Toast.LENGTH_SHORT).show();
+            }
+
+        } else if (view == enable) {
+
+            Intent intent = new Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN);
+            intent.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN, compName);
+            intent.putExtra(DevicePolicyManager.EXTRA_ADD_EXPLANATION, "Additional text explaining why we need this permission");
+            startActivityForResult(intent, RESULT_ENABLE);
+
+        } else if (view == disable) {
+            devicePolicyManager.removeActiveAdmin(compName);
+            disable.setVisibility(View.GONE);
+            enable.setVisibility(View.VISIBLE);
+        } else if (view == start_service) {
+            Toast.makeText(this, "SERVICE STARTING ", Toast.LENGTH_SHORT).show();
+            startService(new Intent(getApplicationContext(), LockService.class));
+        } else if (view == password_lock) {
+            Toast.makeText(this, "password lock ", Toast.LENGTH_SHORT).show();
+            //startService(new Intent(getApplicationContext(), LockService.class));
+            //startActivity(new Intent(MainActivity.this, Password_Activity.class));
+            startActivity(new Intent(MainActivity.this, Main2Activity.class));
+        }
+    }*/
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case RESULT_ENABLE:
+                if (resultCode == Activity.RESULT_OK) {
+                    Toast.makeText(MainActivity.this, "You have enabled the Admin Device features", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Problem to enable the Admin Device features", Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    public void check() {
+        MaterialStyledDialog.Builder dialogHeader_1 = new MaterialStyledDialog.Builder(MainActivity.this);
+        dialogHeader_1.setStyle(Style.HEADER_WITH_TITLE)
+                .withDialogAnimation(true)
+                .setTitle("Allow Access")
+                .setDescription("").withDarkerOverlay(true)
+                .setHeaderColor(R.color.colorPrimaryDark)
+                .setPositiveText("OK")
+                .setCancelable(false)
+                .onPositive(new MaterialDialog.SingleButtonCallback() {
+                    @Override
+                    public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+                        Intent intent = new Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN);
+                        intent.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN, compName);
+                        intent.putExtra(DevicePolicyManager.EXTRA_ADD_EXPLANATION, "Additional text explaining why we need this permission");
+                        startActivityForResult(intent, RESULT_ENABLE);
+                    }
+                }).show();
+    }
+
+    @OnClick({R.id.password_lock, R.id.screen_off})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.password_lock:
+                try {
+                    boolean active = devicePolicyManager.isAdminActive(compName);
+                    if (active) {
+                        if (TextUtils.isEmpty(inputPassword.getText().toString().trim())) {
+                            Toast.makeText(getApplicationContext(), "INPUT REQUIRED", Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+                        DevicePolicyManager devicePolicyManager2 =
+                                (DevicePolicyManager)getSystemService(Context.DEVICE_POLICY_SERVICE);
+                        ComponentName demoDeviceAdmin = new ComponentName(this, MyAdmin.class);
+
+                        devicePolicyManager2.setPasswordQuality(
+                                demoDeviceAdmin,DevicePolicyManager.PASSWORD_QUALITY_UNSPECIFIED);
+                        devicePolicyManager2.setPasswordMinimumLength(demoDeviceAdmin, 2);
+
+                        boolean result = devicePolicyManager2.resetPassword(inputPassword.getText().toString().trim(),
+                                DevicePolicyManager.RESET_PASSWORD_REQUIRE_ENTRY);
+
+
+                        boolean active1 = devicePolicyManager2.isAdminActive(compName);
+
+                        if (active1 && result) {
+                            Stash.put("password",inputPassword.getText().toString().trim());
+                            devicePolicyManager.lockNow();
+                        } else {
+                            Toast.makeText(this, "You need to enable the Admin Device Features", Toast.LENGTH_SHORT).show();
+                        }
+                    } else {
+                        check();
+                    }
+                } catch (Exception e) {
+                    Log.d("Error Line Number", Log.getStackTraceString(e));
+                }
+                break;
+            case R.id.screen_off:
+                try {
+                    boolean active2 = devicePolicyManager.isAdminActive(compName);
+
+                    if (active2) {
+                        devicePolicyManager.lockNow();
+                    } else {
+                        Toast.makeText(this, "You need to enable the Admin Device Features", Toast.LENGTH_SHORT).show();
+                    }
+                } catch (Exception e) {
+                    Log.d("Error Line Number", Log.getStackTraceString(e));
+                }
+                break;
+        }
+    }
+
+    private void loadNativeAd() {
+        // Instantiate a NativeAd object.
+        // NOTE: the placement ID will eventually identify this as your App, you can ignore it for
+        // now, while you are testing and replace it later when you have signed up.
+        // While you are using this temporary code you will only get test ads and if you release
+        // your code like this to the Google Play your users will not receive ads (you will get a no fill error).
+        nativeAd = new NativeAd(this, "1813750035346387_1813751348679589");
+        nativeAd.setAdListener(new NativeAdListener() {
+            @Override
+            public void onMediaDownloaded(Ad ad) {
+                // Native ad finished downloading all assets
+                Log.e(TAG, "Native ad finished downloading all assets.");
+            }
+
+            @Override
+            public void onError(Ad ad, AdError adError) {
+                // Native ad failed to load
+                Log.e(TAG, "Native ad failed to load: " + adError.getErrorMessage());
+            }
+
+            @Override
+            public void onAdLoaded(Ad ad) {
+                if (nativeAd == null || nativeAd != ad) {
+                    return;
+                }
+                // Inflate Native Ad into Container
+                inflateAd(nativeAd);
+            }
+
+            @Override
+            public void onAdClicked(Ad ad) {
+                // Native ad clicked
+                Log.d(TAG, "Native ad clicked!");
+            }
+
+            @Override
+            public void onLoggingImpression(Ad ad) {
+                // Native ad impression
+                Log.d(TAG, "Native ad impression logged!");
+            }
+        });
+
+        // Request an ad
+        nativeAd.loadAd();
+    }
+    private void inflateAd(NativeAd nativeAd) {
+
+        nativeAd.unregisterView();
+
+        // Add the Ad view into the ad container.
+        nativeAdContainer = findViewById(R.id.native_ad_container);
+        LayoutInflater inflater = LayoutInflater.from(MainActivity.this);
+        // Inflate the Ad view.  The layout referenced should be the one you created in the last step.
+        adView = (LinearLayout) inflater.inflate(R.layout.native_ad_layout, nativeAdContainer, false);
+        nativeAdContainer.addView(adView);
+
+        // Add the AdChoices icon
+        LinearLayout adChoicesContainer = findViewById(R.id.ad_choices_container);
+        AdChoicesView adChoicesView = new AdChoicesView(MainActivity.this, nativeAd, true);
+        adChoicesContainer.addView(adChoicesView, 0);
+
+        // Create native UI using the ad metadata.
+        AdIconView nativeAdIcon = adView.findViewById(R.id.native_ad_icon);
+        TextView nativeAdTitle = adView.findViewById(R.id.native_ad_title);
+        MediaView nativeAdMedia = adView.findViewById(R.id.native_ad_media);
+        TextView nativeAdSocialContext = adView.findViewById(R.id.native_ad_social_context);
+        TextView nativeAdBody = adView.findViewById(R.id.native_ad_body);
+        TextView sponsoredLabel = adView.findViewById(R.id.native_ad_sponsored_label);
+        Button nativeAdCallToAction = adView.findViewById(R.id.native_ad_call_to_action);
+
+        // Set the Text.
+        nativeAdTitle.setText(nativeAd.getAdvertiserName());
+        nativeAdBody.setText(nativeAd.getAdBodyText());
+        nativeAdSocialContext.setText(nativeAd.getAdSocialContext());
+        nativeAdCallToAction.setVisibility(nativeAd.hasCallToAction() ? View.VISIBLE : View.INVISIBLE);
+        nativeAdCallToAction.setText(nativeAd.getAdCallToAction());
+        sponsoredLabel.setText(nativeAd.getSponsoredTranslation());
+
+        // Create a list of clickable views
+        List<View> clickableViews = new ArrayList<>();
+        clickableViews.add(nativeAdTitle);
+        clickableViews.add(nativeAdCallToAction);
+
+        // Register the Title and CTA button to listen for clicks.
+        nativeAd.registerViewForInteraction(
+                adView,
+                nativeAdMedia,
+                nativeAdIcon,
+                clickableViews);
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (interstitialAd != null) {
+            interstitialAd.destroy();
+        }
+        super.onDestroy();
+    }
+    private void showAdWithDelay() {
+        /**
+         * Here is an example for displaying the ad with delay;
+         * Please do not copy the Handler into your project
+         */
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                // Check if interstitialAd has been loaded successfully
+                if(interstitialAd == null || !interstitialAd.isAdLoaded()) {
+                    return;
+                }
+                // Check if ad is already expired or invalidated, and do not show ad if that is the case. You will not get paid to show an invalidated ad.
+                if(interstitialAd.isAdInvalidated()) {
+                    return;
+                }
+                // Show the ad
+                interstitialAd.show();
+            }
+        }, 1000 * 60 * 15); // Show the ad after 15 minutes
+    }
+}
